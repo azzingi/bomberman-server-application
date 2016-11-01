594a59f3c5b50c00dcbfc8cd8e85cc81c43fefd6
Nathanael Weber <nathanael.weber@bbcag.ch>
2016-11-01 10:44:41 +0100

    added start of Server

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,13 @@
 package application;
 
+import network.Server.ServerStub;
+import network.ServerApplicationHandler;
+
 /**
  * Created by Nathanael on 18.10.2016.
  */
 public class App {
     public static void main(String[] args) {
-
+        ServerStub server = new ServerStub(new ServerApplicationHandler());
     }
 }
