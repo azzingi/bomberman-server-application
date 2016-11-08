95d4a290cb6334cdbb8affda65af7b70abfe123d
Nathanael Weber <nathanael.weber@bbcag.ch>
2016-11-08 10:09:54 +0100

    added StartGameHandler and UpdateHandler

---
+++ src/network/HandlerClasses/StartGameHandler.java
@@ -0,0 +1,14 @@
+package network.HandlerClasses;
+
+import network.Handler;
+import network.Message;
+
+/**
+ * Created by Nathanael on 08.11.2016.
+ */
+public class StartGameHandler extends Handler {
+    @Override
+    public void handle(Message msg) {
+
+    }
+}
---
+++ src/network/HandlerClasses/UpdateHandler.java
@@ -0,0 +1,14 @@
+package network.HandlerClasses;
+
+import network.Handler;
+import network.Message;
+
+/**
+ * Created by Nathanael on 08.11.2016.
+ */
+public class UpdateHandler extends Handler {
+    @Override
+    public void handle(Message msg) {
+
+    }
+}
