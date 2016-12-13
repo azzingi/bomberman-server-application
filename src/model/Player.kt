39a7a547f8f9abc3d03abb4cb717ecd428bb01a8
Nathanael Weber <nathanael.weber@bbcag.ch>
2016-12-13 10:40:20 +0100

    kotlin hello

---
+++ src/model/Player.kt
@@ -0,0 +1,22 @@
+package model
+
+/**
+ * Created by Nathanael on 01.11.2016.
+ */
+class Player(val name: String, start: Tile) {
+    var isAlive: Boolean = false
+    var tile: Tile? = null
+        private set
+
+    init {
+        tile = start
+    }
+
+    fun move(tile: Tile) {
+        this.tile = tile
+    }
+
+    fun placeBomb() {
+
+    }
+}
--- src/network/Server/ServerStub.java
+++ src/network/Server/ServerStub.java
@@ -16,21 +16,21 @@ import network.server.ServerApplicationInterface;
  * simuliert. In der Methode deliverMessageToServer() kann programmiert werden, welche Meldungen
  * dies konkret sind. Die Methode wartet mit der Auslieferung, bis der Benutzer die Taste
  * ENTER dr�ckt. Dies kann je nach Situation hilfreich sein beim Testen. Die Methode kann aber
- * beliebig an die Bed�rfnisse des Tests angepasst werden. 
+ * beliebig an die Bed�rfnisse des Tests angepasst werden.
  * Der Server-Komponente selber stehen die send-Methode und die broadcast-Methode zur Verf�gung,
  * um eine Antwort an einen oder alle Clients zu simulieren. Die Meldung wird lediglich auf der
  * Konsole ausgegeben.
- * 
+ *
  * @author Andres Scheidegger
  *
  */
 public class ServerStub extends Server {
-  
+
   /**
    * Konstruktor. Muss von der Server-Komponente aufgerufen werden. Startet einen separaten Thread
    * zum Simulieren der Meldungen, welche von den Clients empfangen werden.
    * @see #deliverMessagesToServer()
-   * @param serverApplication Eine Referenz auf ein Objekt der Server-Komponente, welches das 
+   * @param serverApplication Eine Referenz auf ein Objekt der Server-Komponente, welches das
    *                          ServerApplicationInterface implementiert.
    */
   public ServerStub(ServerApplicationInterface serverApplication) {
@@ -46,8 +46,7 @@ public class ServerStub extends Server {
 
   /**
    * Kann von der Server-Komponente aufgerufen werden, um den Versand einer Meldung an
-   * einen bestimmten Client zu simulieren. 
-   * @see network.client.Server#send(network.Message, String)
+   * einen bestimmten Client zu simulieren.
    */
   @Override
   public void send(Message message, String connectionId) {
@@ -56,16 +55,15 @@ public class ServerStub extends Server {
 
   /**
    * Kann von der Server-Komponente aufgerufen werden, um den Versand einer Meldung an
-   * alle Clients zu simulieren. 
-   * @see network.client.Server#broadcast(network.Message)
+   * alle Clients zu simulieren.
    */
   @Override
   public void broadcast(Message message) {
     System.out.println(message);
   }
-  
+
   /**
-   * Wartet auf eine Eingabe von der Konsole (ENTER-Taste). Anschliessend werden die weiteren 
+   * Wartet auf eine Eingabe von der Konsole (ENTER-Taste). Anschliessend werden die weiteren
    * Anweisungen ausgef�hrt. Z.B. k�nnen Meldungen an den Server ausgeliefert werden, indem die Methode
    * handleMessage() bei der Server-Komponente aufgerufen wird. Diese Methode kann den Bed�rfnissen
    * des Tests angepasst werden.
